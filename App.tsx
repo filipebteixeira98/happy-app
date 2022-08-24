f413277b3c7ff818b0612c92d0e9dae3d642dd14
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-08-24 20:16:49 -0300

    chore: configured fonts

--- App.tsx
+++ App.tsx
@@ -8,10 +8,26 @@ import {
 } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
+import { useFonts } from 'expo-font';
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from '@expo-google-fonts/nunito';
 
 import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -75,6 +91,7 @@ const styles = StyleSheet.create({
   calloutText: {
     color: '#0089a5',
     fontSize: 14,
+    fontFamily: 'Nunito_700Bold',
   },
   footer: {
     position: 'absolute',
@@ -91,6 +108,7 @@ const styles = StyleSheet.create({
     elevation: 3,
   },
   footerText: {
+    fontFamily: 'Nunito_700Bold',
     color: '#8fa7b3',
   },
   createOrphanageButton: {
