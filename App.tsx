046e2481647e08b6a79bbdac57192e10dc56f2e9
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-08-24 19:47:43 -0300

    chore: created home page with map that has a marker inside of it

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: -12.9913412,
+          longitude: -38.5165131,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+        style={styles.map}
+      >
+        <Marker
+          icon={mapMarker}
+          coordinate={{
+            latitude: -12.9913412,
+            longitude: -38.5165131,
+          }}
+        />
+      </MapView>
     </View>
   );
 }
@@ -13,8 +33,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
